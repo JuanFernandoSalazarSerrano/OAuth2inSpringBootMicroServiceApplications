5a4552b02a1caab9963cd29c15570f5f0e9745bc
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2026-02-15 21:47:52 -0500

    resource server controller port test

--- ResourceServer/src/main/java/com/appsdeveloper/ws/api/ResourceServer/Controller/UsersController.java
+++ ResourceServer/src/main/java/com/appsdeveloper/ws/api/ResourceServer/Controller/UsersController.java
@@ -2,6 +2,8 @@ package com.appsdeveloper.ws.api.ResourceServer.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/users")
 public class UsersController {
 
+    // enviroment object
+    @Autowired
+    Environment env;
+
     @GetMapping("/status/check")
     public String status(){
-        return "Working...";
+        return "Working..." + env.getProperty("local.server.port");
     }
 }
