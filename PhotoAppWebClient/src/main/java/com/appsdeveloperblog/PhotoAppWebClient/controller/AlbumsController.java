d2fa6c25199133a7250ce5258ee7baec7694df09
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2026-02-22 17:09:56 -0500

    controller comments

--- PhotoAppWebClient/src/main/java/com/appsdeveloperblog/PhotoAppWebClient/controller/AlbumsController.java
+++ PhotoAppWebClient/src/main/java/com/appsdeveloperblog/PhotoAppWebClient/controller/AlbumsController.java
@@ -1,9 +1,16 @@
 package com.appsdeveloperblog.PhotoAppWebClient.controller;
 
+
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -15,15 +22,35 @@ import com.appsdeveloperblog.PhotoAppWebClient.response.AlbumRest;
 @Controller
 public class AlbumsController {
 
+    // create an oauth2ClientService object to interact with the access token 
+	@Autowired
+	OAuth2AuthorizedClientService oauth2ClientService;
+
     @GetMapping("/albumsjava")
-    public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+    public String getAlbums(Model model,
+        // @AuthenticationPrincipal injects the principal in its specified implementation in this case, open id connect
+         @AuthenticationPrincipal OidcUser principal) { 
 
-        System.out.println("principal " + principal);
-
-        OidcIdToken idToken = principal.getIdToken();
-        String idTokenValue = idToken.getTokenValue();
-
-        System.out.println("Get token value " + idTokenValue);
+		// use securitycontextholder to access the security context and obtain the access token to identify the currently authenticated user 
+		Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+        
+        // cast the abstract authentication access token to an especific implementation, in this case outh2 (could be, okta, google, facebook, etc) 
+		OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+		
+        // this will be used to authorize this client object with the access token and the username
+		OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), 
+				oauthToken.getName());
+		
+        // gettoken value returns an string to display it with sout
+		String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+		System.out.println("jwtAccessToken = " + jwtAccessToken);
+		
+		
+		System.out.println("Principal = " + principal);
+		
+		OidcIdToken idToken = principal.getIdToken();
+		String idTokenValue = idToken.getTokenValue();
+		System.out.println("idTokenValue = " + idTokenValue);
 
 
         AlbumRest album = new AlbumRest();
