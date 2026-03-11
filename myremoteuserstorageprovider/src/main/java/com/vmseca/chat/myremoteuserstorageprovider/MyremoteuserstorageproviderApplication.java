734e143843de4396a41c86fc8d6813b3a4e77121
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2026-03-11 08:33:57 -0500

    remote user storage provider

---
+++ myremoteuserstorageprovider/src/main/java/com/vmseca/chat/myremoteuserstorageprovider/MyremoteuserstorageproviderApplication.java
@@ -0,0 +1,13 @@
+package com.vmseca.chat.myremoteuserstorageprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyremoteuserstorageproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyremoteuserstorageproviderApplication.class, args);
+	}
+
+}
---
+++ myremoteuserstorageprovider/src/test/java/com/vmseca/chat/myremoteuserstorageprovider/MyremoteuserstorageproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmseca.chat.myremoteuserstorageprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyremoteuserstorageproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
